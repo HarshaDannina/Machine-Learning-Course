c79ac6cad0969e9ccf07be0c51121935b034db4e
Harsha Manoj <danninamanoj123@gmail.com>
2018-12-02 01:26:03 +0530

    Update Assignment1.py

--- Assignments/Python/Assignment1.py
+++ Assignments/Python/Assignment1.py
@@ -2,13 +2,16 @@
 #Enter 2 numbers and output will be 4 arthematic operations of two numbers.
 
 lis = input("Enter any two numbers: ").split(' ')
+try:
+  
+  n1 = int(lis[0])+int(lis[1])
+  n2 = int(lis[0])-int(lis[1])
+  n3 = int(lis[0])*int(lis[1])
+  n4 = int(lis[0])/int(lis[1])
 
-n1 = int(lis[0])+int(lis[1])
-n2 = int(lis[0])-int(lis[1])
-n3 = int(lis[0])*int(lis[1])
-n4 = int(lis[0])/int(lis[1])
-
-print(n1)
-print(n2)
-print(n3)
-print(n4)
+  print(n1)
+  print(n2)
+  print(n3)
+  print(n4)
+except:
+  print("Sorry: We can't divide with zero, try with another value")
